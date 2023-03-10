af15c397a835df42e871f4d16dcff579eb8405f4
Anna Techkina <an.techkina@mail.ru>
2023-03-10 18:18:27 +0400

    Задача 1

--- Techkina_Anna_dz/task_1/Program.cs
+++ Techkina_Anna_dz/task_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+  Console.WriteLine("max " + a);
+}
+else
+{
+  Console.WriteLine("max " + b);
+}
